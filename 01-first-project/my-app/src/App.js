e413100c9aefcfc814bd6645fa42ca45e4da77b4
Станислав <stanisla777@mail.ru>
2019-08-15 01:21:30 +0300

    Add

--- 01-first-project/my-app/src/App.js
+++ 01-first-project/my-app/src/App.js
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+import Fun_header from './Header';
+import Fun_footer from './Footer';
+
+const App = () =>
+{
+  return(
+      <div className="container App">
+          <Fun_header/>
+          <Fun_footer/>
+      </div>
+  )
+};
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
+
---
+++ 01-first-project/my-app/src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import App from "./App";
+
+const Fun_footer = () =>
+{
+    return(
+        <footer>
+            <p>Это классный футер</p>
+        </footer>
+    )
+};
+
+export default Fun_footer;
